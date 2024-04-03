2e83f8c3737b4ad2eb5f9ee20cb69486805df816
temo <teimurazi.g@gmail.com>
2024-04-04 00:57:04 +0400

    add experiment lux ventus

---
+++ index.php
@@ -0,0 +1,282 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>lux ventus</title>
+    <link rel="stylesheet" href="./styles/reset.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+      rel="stylesheet"
+    />
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+      integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link rel="stylesheet" href="./styles/style.css" />
+  </head>
+
+  <body>
+    <header>
+      <div class="navbar-wrapper">
+        <a class="logo" href=""><img src="./images/heder_logo.svg" alt="" /></a>
+
+        <nav>
+          <ul>
+            <li><a href="index.html">Fashion</a></li>
+            <li><a href="">Lifestyle</a></li>
+            <li><a href="">Spiritual</a></li>
+            <li><a href="index3.html">Mental Health</a></li>
+
+            <button class="button1">Contact Us</button>
+            <button class="searchDiv">
+              <img style="width: 24px;" src="./images/search icon.png" alt="">
+            </button>
+          </ul>
+        </nav>
+      </div>
+
+      <!-- <div class="blackTag">ENSEMBLES</div>
+
+        <p class="arrowIcon"><i class="fa-solid fa-arrow-right"></i><span style="padding-left: 10px;">READ MORE</span></p> -->
+    </header>
+
+    <!-- section 1 -->
+
+    <section class="section1">
+      <div class="section1Texts">
+        <p>LUX VENTUS MAGAZINE</p>
+        <h1>We Are the <span>Ventus Fashion</span></h1>
+        <p>
+          We like to gossip about everything, but we will<br />
+          never forget the daily fashion dose.
+        </p>
+      </div>
+    </section>
+
+    <!-- section 2 -->
+    <section class="section2">
+      <h2>Topics</h2>
+      <div class="sect2Wrap">
+        <div class="sect2Cont">
+          <img src="./images/icon_1.svg" alt="" />
+          <p>Lingerie</p>
+          <button class="button1">View More</button>
+        </div>
+        <div class="sect2Cont">
+          <img src="./images/icon_2.svg" alt="" />
+          <p>Lingerie</p>
+          <button class="button1">View More</button>
+        </div>
+        <div class="sect2Cont">
+          <img src="./images/icon_3.svg" alt="" />
+          <p>Lingerie</p>
+          <button class="button1">View More</button>
+        </div>
+        <div class="sect2Cont">
+          <img src="./images/icon_4.svg" alt="" />
+          <p>Lingerie</p>
+          <button class="button1">View More</button>
+        </div>
+        <div class="sect2Cont">
+          <img src="./images/icon_5.svg" alt="" />
+          <p>Lingerie</p>
+          <button class="button1">View More</button>
+        </div>
+        <div class="sect2Cont">
+          <img src="./images/icon_6.svg" alt="" />
+          <p>Lingerie</p>
+          <button class="button1">View More</button>
+        </div>
+      </div>
+    </section>
+
+    <div class="line"></div>
+
+    <!-- section 3 -->
+
+    <section class="section3">
+      <div class="sec3Cont">
+        <div class="blackTag">FASHION</div>
+        <img class="img1_blog" src="./images/1_blog.svg" alt="1_blog" />
+        <div class="sec3insidesection">
+          <div class="userBarAndH4">
+            <div class="userBar">
+              <img src="./images/user.svg" alt="user" />
+              <h3>Mallory Reyn</h3>
+              <h3>April 14th, 2023</h3>
+            </div>
+            <h4 class="cominginhot">COMING IN HOT!</h4>
+          </div>
+          <h5 class="h5">The Latest Trends from the Moon Fashion Week.</h5>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa
+            mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien
+            fringilla, mattis ligula consectetur, ultrices mauris. Maecenas
+            vitae mattis tellus. Nullam quis imperdiet augue. Vestibulum.
+          </p>
+        </div>
+        <p class="arrowIcon">
+          <i class="fa-solid fa-arrow-right"></i
+          ><span style="padding-left: 10px">READ MORE</span>
+        </p>
+      </div>
+    </section>
+
+    <div class="section4">
+      <h2>Trending</h2>
+      <div class="sec4cards">
+        <div class="sec4card">
+          <img class="cardFirstImg" src="./images/section4img1.png" alt="2_blog" />
+          <div class="userBar sec4 #sec4">
+            <img src="./images/user.svg" alt="user" />
+            <h3>Mallory Reyn</h3>
+            <h3>April 14th, 2023</h3>
+          </div>
+          <h5 class="h5">
+            10 Things to help you with<br />
+            social stress.
+          </h5>
+          <p class="sec4p">
+            Lorem ipsum dolor sit amet, consectetur Pellentesque sit amet sapien
+            fringilla, mattis mattis tellus. Nullam quis imperdiet augue.
+            Vestibulum.
+          </p>
+          <p class="arrowIcon">
+            <i class="fa-solid fa-arrow-right"></i
+            ><span style="padding-left: 10px">READ MORE</span>
+          </p>
+          <div class="imgtag">
+            <h2>LIFESTYLE</h2>
+          </div>
+        </div>
+        <div class="sec4card">
+          <img class="cardFirstImg" src="./images/section4img2.png" alt="3_blog" />
+          <div class="userBar sec4">
+            <img src="./images/user.svg" alt="user" />
+            <h3>Mallory Reyn</h3>
+            <h3>April 14th, 2023</h3>
+          </div>
+          <h5 class="h5">
+            10 Things to help you with<br />
+            social stress.
+          </h5>
+          <p class="sec4p">
+            Lorem ipsum dolor sit amet, consectetur Pellentesque sit amet sapien
+            fringilla, mattis mattis tellus. Nullam quis imperdiet augue.
+            Vestibulum.
+          </p>
+          <p class="arrowIcon">
+            <i class="fa-solid fa-arrow-right"></i
+            ><span style="padding-left: 10px">READ MORE</span>
+          </p>
+          <div class="imgtag">
+            <h2>SUMMER</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- section 5 -->
+    <section class="section5">
+      <div class="section5card">
+        <img src="./images/4_blog.svg" alt="" />
+        <div class="card-content">
+          <div class="user-info">
+            <img src="./images/user.svg" alt="" />
+            <div class="user-text">
+              <p>Mallory Reyn</p>
+              <p>April 14th, 2023</p>
+            </div>
+          </div>
+          <h3>5 Reasons to avoid shopping on saturdays</h3>
+          <p class="card-description">
+            Lorem ipsum dolor sit amet, consectetur Pellentesque sit amet sapien
+            fringilla, mattis mattis tellus. Nullam quis imperdiet augue.
+            Vestibulum.
+          </p>
+          <p class="arrowIcon">
+            <i class="fa-solid fa-arrow-right"></i
+            ><span style="padding-left: 10px">READ MORE</span>
+          </p>
+        </div>
+        <div class="blackTag">FASHION</div>
+      </div>
+      <div class="section5card">
+        <img src="./images/5_blog.svg" alt="" />
+        <div class="card-content">
+          <div class="user-info">
+            <img src="./images/user.svg" alt="" />
+            <div class="user-text">
+              <p>Mallory Reyn</p>
+              <p>April 14th, 2023</p>
+            </div>
+          </div>
+          <h3>10 Brands that remained faithful to your classy look.</h3>
+          <p class="card-description">
+            Lorem ipsum dolor sit amet, consectetur Pellentesque sit amet sapien
+            fringilla, mattis mattis tellus. Nullam quis imperdiet augue.
+            Vestibulum.
+          </p>
+          <p class="arrowIcon">
+            <i class="fa-solid fa-arrow-right"></i
+            ><span style="padding-left: 10px">READ MORE</span>
+          </p>
+        </div>
+        <div class="blackTag">ENSEMBLES</div>
+      </div>
+      <div class="section5card">
+        <img src="./images/6_blog.svg" alt="" />
+        <div class="card-content">
+          <div class="user-info">
+            <img src="./images/user.svg" alt="" />
+            <div class="user-text">
+              <p>Mallory Reyn</p>
+              <p>April 14th, 2023</p>
+            </div>
+          </div>
+          <h3>Mental health tips to cope with stress.</h3>
+          <p class="card-description">
+            Lorem ipsum dolor sit amet, consectetur Pellentesque sit amet sapien
+            fringilla, mattis mattis tellus. Nullam quis imperdiet augue.
+            Vestibulum.
+          </p>
+          <p class="arrowIcon">
+            <i class="fa-solid fa-arrow-right"></i
+            ><span style="padding-left: 10px">READ MORE</span>
+          </p>
+        </div>
+        <div class="blackTag">MENTAL HEALTH</div>
+      </div>
+    </section>
+
+    <footer>
+      <div class="footerDiv">
+        <div class="footerDivInside">
+          <div class="footerIconLogo">
+            <a class="logo" href=""
+              ><img src="./images/heder_logo.svg" alt=""
+            /></a>
+
+            <div class="footer-icons">
+              <h2>Let’s Hang Out</h2>
+              <img src="./images/facebook.svg" alt="icon1" />
+              <img src="./images/twitter.svg" alt="icon2" />
+              <img src="./images/linkedin.svg" alt="icon3" />
+              <img src="./images/youtube.svg" alt="icon4" />
+              <img src="./images/instagram.svg" alt="icon4" />
+            </div>
+          </div>
+
+          <div class="line"></div>
+          <p class="copyrightFooter">© 2023 - Lux Ventus by Zine.E.Falouti</p>
+        </div>
+      </div>
+    </footer>
+  </body>
+</html>
